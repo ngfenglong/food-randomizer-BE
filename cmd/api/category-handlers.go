819c4c9d71d901f0223e8c408efc6d02af65981a
fenglong_ng <fenglong1502@hotmail.com>
2023-04-22 13:39:45 +0800

    Added some endpoint and implementation for Categories

--- cmd/api/category-handlers.go
+++ cmd/api/category-handlers.go
@@ -7,7 +7,18 @@ func (app *application) getCategoryByID(w http.ResponseWriter, r *http.Request) 
 }
 
 func (app *application) getAllCategories(w http.ResponseWriter, r *http.Request) {
+	categories, err := app.models.DB.GetAllCategories()
 
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.WriteJSON(w, http.StatusOK, categories, "categories")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) editCategory(w http.ResponseWriter, r *http.Request) {
--- models/category-db.go
+++ models/category-db.go
@@ -1,1 +1,59 @@
 package models
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func (m *DBModel) GetCategoryByID(id int) (*Category, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `select id, category_name, created_at, updated_at from category where id = ?`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+	var category Category
+	err := row.Scan(
+		&category.ID,
+		&category.CategoryName,
+		&category.CreatedAt,
+		&category.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &category, nil
+}
+
+func (m *DBModel) GetAllCategories() ([]*Category, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := fmt.Sprintf(`select id, category_name, created_at, updated_at from category`)
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var categories []*Category
+	for rows.Next() {
+		var category Category
+		err := rows.Scan(
+			&category.ID,
+			&category.CategoryName,
+			&category.CreatedAt,
+			&category.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		categories = append(categories, &category)
+	}
+	return categories, nil
+
+}
